3df52b121eb285f3bc13fb123102db22cc0484a1
Gunnar Oledal <gunnar.oledal@gmail.com>
2024-05-02 15:36:00 +0200

    WIP: placing things in the world

--- src/views/WallTests.vue
+++ src/views/WallTests.vue
@@ -19,6 +19,8 @@ function placeCursor(evt: DetailEvent<{ intersection: THREE.Intersection }>) {
 }
 
 function onClick(evt: DetailEvent<{ intersection: THREE.Intersection }>) {
+  releasePlacedObject();
+  return;
   // console.log(evt);
   const scene = sceneTag.value!;
   const newTorus = document.createElement('a-torus');
@@ -34,16 +36,48 @@ function onClick(evt: DetailEvent<{ intersection: THREE.Intersection }>) {
   scene.appendChild(newTorus);
 }
 
+
+const placedObjectsEntity = ref<Entity>();
+function releasePlacedObject() {
+  cursorEntity.value?.flushToDOM(true);
+  // const placedEntity = cursorEntity.value?.firstChild as Entity;
+  // placedEntity.flushToDOM();
+  placedObjectsEntity.value?.append(...cursorEntity.value?.children);
+  cursorEntity.value?.replaceChildren()
+}
+
+type placeableAssetTypes = `a-${'image' | 'sphere'}`;
+function createPlacedObject(type: placeableAssetTypes, url: string) {
+  console.log(type, url);
+  const newEntity = document.createElement(type)
+  if (type === 'a-image') {
+    newEntity.setAttribute('src', url);
+  } else {
+    newEntity.setAttribute('color', 'teal');
+  }
+  cursorEntity.value?.replaceChildren(newEntity);
+}
+
 </script>
 
 <template>
+  <div id="ui-overlay" class="absolute z-50 left-5 top-5">
+    <button class="p-3 text-white rounded-md cursor-pointer bg-zinc-800"
+      @click="createPlacedObject('a-image', '/photos/joey-chacon-edbYu4vxXww-unsplash.jpg')"> Hello</button>
+  </div>
   <a-scene ref="sceneTag" cursor="rayOrigin: mouse;" raycaster="objects: .clickable;" raycaster-update
     @raycast-update="placeCursor">
     <a-assets>
       <a-asset-item id="sponza" :src="sponzaUrl"></a-asset-item>
     </a-assets>
-    <a-torus ref="cursorEntity" scale=".1 .1 .1" color="teal" />
-    <a-gltf-model @click="onClick" @raycast-change="placeCursor" class="clickable" src="#sponza" />
+    <a-entity ref="placedObjectsEntity">
+
+    </a-entity>
+    <a-entity ref="cursorEntity">
+      <a-torus scale=".1 .1 .1" color="teal" />
+    </a-entity>
+
+    <a-gltf-model @click="onClick" class="clickable" src="#sponza" />
     <a-torus-knot position="0 1.6 -4" color="red"
       animation="property: rotation; from: 0 0 0; to: 400 300 500; dir: alternate; dur: 15000; easing: easeInOutQuad; loop: true;"></a-torus-knot>
   </a-scene>
