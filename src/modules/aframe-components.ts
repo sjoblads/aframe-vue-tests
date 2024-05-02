392d541c425f9cf257fedd54a789b89625aa93c1
Gunnar Oledal <gunnar.oledal@gmail.com>
2024-05-02 10:58:09 +0200

    POC placing stuff with raycaster

--- src/modules/aframe-components.ts
+++ src/modules/aframe-components.ts
@@ -2,6 +2,7 @@ import meshUi from "./aframe-components/mesh-ui";
 import makeGltfSwappable from "./aframe-components/make-gltf-swappable";
 import 'aframe'
 import modelColor from './aframe-components/model-color';
+import raycasterListen from "./aframe-components/raycaster-listen";
 // import 'aframe-troika-text'
 // import 'https://github.com/PyryM/aframe-pano-portal/raw/master/dist/aframe-pano-portal.min.js'
 // import './aframe-pano-portal';
@@ -15,6 +16,7 @@ export default async function registerComponents() {
   meshUi();
   modelColor();
   makeGltfSwappable();
+  raycasterListen();
   // gltfShadow();
   // const troika = await import('./aframe-components/troika-text');
   // troika.default();
--- src/views/WallTests.vue
+++ src/views/WallTests.vue
@@ -1,8 +1,48 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import sponzaUrl from '@/assets/sponza.glb?url'
+import { type DetailEvent, THREE, type Entity, type Scene } from 'aframe';
+
+const sceneTag = ref<Scene>();
+
+const cursorEntity = ref<Entity>();
+function placeCursor(evt: DetailEvent<{ intersection: THREE.Intersection }>) {
+  // console.log(evt.detail.intersection.point);
+  if (!cursorEntity.value) return;
+  cursorEntity.value.object3D.position.set(...evt.detail.intersection.point.toArray());
+  const normal = evt.detail.intersection.normal;
+  if (!normal) return;
+  const up = new THREE.Vector3(0, 0, 1);
+  const newRotation = new THREE.Quaternion();
+  newRotation.setFromUnitVectors(up, normal);
+  cursorEntity.value.object3D.rotation.setFromQuaternion(newRotation);
+}
+
+function onClick(evt: DetailEvent<{ intersection: THREE.Intersection }>) {
+  // console.log(evt);
+  const scene = sceneTag.value!;
+  const newTorus = document.createElement('a-torus');
+  newTorus.setAttribute('color', 'blue');
+  newTorus.setAttribute('scale', '.1 .1 .1');
+  newTorus.object3D.position.set(...evt.detail.intersection.point.toArray());
+  const normal = evt.detail.intersection.normal;
+  if (!normal) return;
+  const up = new THREE.Vector3(0, 0, 1);
+  const newRotation = new THREE.Quaternion();
+  newRotation.setFromUnitVectors(up, normal);
+  newTorus.object3D.rotation.setFromQuaternion(newRotation);
+  scene.appendChild(newTorus);
+}
+
 </script>
 
 <template>
-  <a-scene>
+  <a-scene ref="sceneTag" cursor="rayOrigin: mouse;" raycaster="objects: .clickable;">
+    <a-assets>
+      <a-asset-item id="sponza" :src="sponzaUrl"></a-asset-item>
+    </a-assets>
+    <a-torus ref="cursorEntity" scale=".1 .1 .1" color="teal" />
+    <a-gltf-model @click="onClick" raycaster-listen @raycast-change="placeCursor" class="clickable" src="#sponza" />
     <a-torus-knot position="0 1.6 -4" color="red"
       animation="property: rotation; from: 0 0 0; to: 400 300 500; dir: alternate; dur: 15000; easing: easeInOutQuad; loop: true;"></a-torus-knot>
   </a-scene>
