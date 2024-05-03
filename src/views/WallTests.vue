d817f92970806add64fb0e752fca31cdd4cb337b
Gunnar Oledal <gunnar.oledal@gmail.com>
2024-05-03 09:36:12 +0200

    POC: Possible to position image in environment. Yey!

--- src/views/WallTests.vue
+++ src/views/WallTests.vue
@@ -19,35 +19,44 @@ function placeCursor(evt: DetailEvent<{ intersection: THREE.Intersection }>) {
 }
 
 function onClick(evt: DetailEvent<{ intersection: THREE.Intersection }>) {
-  releasePlacedObject();
+  addCursorChildrenToScene();
   return;
-  // console.log(evt);
-  const scene = sceneTag.value!;
-  const newTorus = document.createElement('a-torus');
-  newTorus.setAttribute('color', 'blue');
-  newTorus.setAttribute('scale', '.1 .1 .1');
-  newTorus.object3D.position.set(...evt.detail.intersection.point.toArray());
-  const normal = evt.detail.intersection.normal;
-  if (!normal) return;
-  const up = new THREE.Vector3(0, 0, 1);
-  const newRotation = new THREE.Quaternion();
-  newRotation.setFromUnitVectors(up, normal);
-  newTorus.object3D.rotation.setFromQuaternion(newRotation);
-  scene.appendChild(newTorus);
 }
 
 
 const placedObjectsEntity = ref<Entity>();
-function releasePlacedObject() {
-  cursorEntity.value?.flushToDOM(true);
-  // const placedEntity = cursorEntity.value?.firstChild as Entity;
-  // placedEntity.flushToDOM();
-  placedObjectsEntity.value?.append(...cursorEntity.value?.children);
+// NOTE: aframe is acting a bit peculiar, in where it's not possible to simply reparent a DOM entity.
+// We need to clone it in order to make aframe pickup that it should be included in the scene
+function addCursorChildrenToScene() {
+  const cursor = cursorEntity.value;
+  if (!cursor) {
+    console.warn('cursorEntity was undefined');
+    return;
+  }
+  // Unclear if we need to flushToDom
+  // I think it might become necessary if we want to keep custom properties set in components.
+  // cursorEntity.value?.flushToDOM(true);
+  const cursorPos = cursor.object3D.getWorldPosition(new THREE.Vector3());
+  const cursorRot = cursor.object3D.getWorldQuaternion(new THREE.Quaternion());
+
+  const clone = cursorEntity.value?.cloneNode(true);
+  const children = clone?.childNodes;
   cursorEntity.value?.replaceChildren()
+
+  if (!children) {
+    console.warn('no children in cursor');
+    return;
+  }
+  children?.forEach(node => {
+    const e = node as Entity;
+    e.object3D.position.add(cursorPos);
+    e.object3D.setRotationFromQuaternion(cursorRot);
+  })
+  placedObjectsEntity.value?.append(...children);
 }
 
 type placeableAssetTypes = `a-${'image' | 'sphere'}`;
-function createPlacedObject(type: placeableAssetTypes, url: string) {
+function createCursorChild(type: placeableAssetTypes, url: string) {
   console.log(type, url);
   const newEntity = document.createElement(type)
   if (type === 'a-image') {
@@ -63,7 +72,7 @@ function createPlacedObject(type: placeableAssetTypes, url: string) {
 <template>
   <div id="ui-overlay" class="absolute z-50 left-5 top-5">
     <button class="p-3 text-white rounded-md cursor-pointer bg-zinc-800"
-      @click="createPlacedObject('a-image', '/photos/joey-chacon-edbYu4vxXww-unsplash.jpg')"> Hello</button>
+      @click="createCursorChild('a-image', '/photos/joey-chacon-edbYu4vxXww-unsplash.jpg')"> Hello</button>
   </div>
   <a-scene ref="sceneTag" cursor="rayOrigin: mouse;" raycaster="objects: .clickable;" raycaster-update
     @raycast-update="placeCursor">
