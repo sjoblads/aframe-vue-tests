68b51ffd8486e490cb8a394f0c50e03768b1c7b4
Gunnar Oledal <gunnar.oledal@gmail.com>
2024-05-06 15:30:29 +0200

    only tick when transform is changed (rotation and scale)

--- src/modules/aframe-components/box-helper.ts
+++ src/modules/aframe-components/box-helper.ts
@@ -1,4 +1,4 @@
-import { THREE } from "aframe";
+import { THREE, type Entity } from "aframe";
 
 export default function () {
   AFRAME.registerComponent('box-helper', {
@@ -9,14 +9,32 @@ export default function () {
       color: { type: 'color', default: '#ffffff' }
     },
     init: function () {
+      // this.tick = AFRAME.utils.throttleTick(this.tick, 100, this);
       this.boxHelper = new THREE.BoxHelper(this.el.object3D)
-      this.boxHelper.material.linewidth = 5;
+
       const scene = this.el.sceneEl?.object3D
       if (scene) {
         scene.add(this.boxHelper);
       }
     },
+    tick: (function () {
+      let prevScale = new THREE.Vector3();
+      let prevQuat = new THREE.Quaternion();
+      let currentQuat = new THREE.Quaternion();
 
+      return function () {
+        const el = this.el as Entity;
+        const currentScale = el.object3D.scale.clone();
+        currentQuat.setFromEuler(el.object3D.rotation);
+        if (currentScale.distanceTo(prevScale) > 0.01 || currentQuat.angleTo(prevQuat) > 0.01) {
+          // console.log('UPDATING');
+          this.boxHelper.update();
+        }
+
+        prevScale.copy(currentScale);
+        prevQuat.copy(currentQuat);
+      };
+    })(),
     update: function () {
       this.boxHelper.visible = this.data.enabled;
       if (this.data.color && this.boxHelper) {
